939ebb0f0934e239d9d12fa0b09d4fc9d2f548e3
hosne mubarak <h.m.tasmir.cse@gmail.com>
2022-01-20 20:14:15 +0600

    add unittest

--- test.py
+++ test.py
@@ -1,9 +1,46 @@
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print('FooBar')
-    elif i % 3 == 0:
-        print('Foo')
-    elif i % 5 == 0:
-        print('Bar')
-    else:
-        print(i)
+from unittest import TestCase
+
+
+def print_foobar():
+    my_list = []
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            my_list.append('FooBar')
+        elif i % 3 == 0:
+            my_list.append('Foo')
+        elif i % 5 == 0:
+            my_list.append('Bar')
+        else:
+            my_list.append(i)
+    return my_list
+
+
+print(print_foobar())
+
+
+def is_foo(n):
+    return print_foobar()[n - 1] == 'Foo'
+
+
+def is_bar(n):
+    return print_foobar()[n - 1] == 'Bar'
+
+
+def is_foobar(n):
+    return is_foo(n) == is_bar(n)
+
+
+class TestIsFoo(TestCase):
+
+    def test_is_foo(self):
+        self.assertTrue(is_foo(3))
+
+
+class TestIsBar(TestCase):
+    def test_is_bar(self):
+        self.assertTrue(is_bar(10))
+
+
+class TestIsFoobar(TestCase):
+    def test_is_foobar(self):
+        self.assertTrue(is_foobar(15))
